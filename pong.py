62c7562873179af32ce31b002c7c9ecb0a4b606a
Dani Donisa <dani.doni@protonmail.com>
2023-09-26 21:43:52 +0200

    Implement basic collisions with the boundaries

--- pong.py
+++ pong.py
@@ -1,6 +1,5 @@
-import os
-import random
 from typing import List
+from math import sin, cos, radians
 
 # import basic pygame modules
 import pygame as pg
@@ -28,26 +27,54 @@ class Paddle:
 
 
 class Ball:
-    def __init__(self, position: pg.Vector2, velocity: pg.Vector2) -> None:
+    def __init__(self, position: pg.Vector2, speed, angle) -> None:
+        self.speed = speed
+        # In degrees, 0 is down, 90 to the right
+        self.angle = angle
         self.position: pg.Vector2 = position
-        self.velocity: pg.Vector2 = velocity
+
+    def velocity(self) -> pg.Vector2:
+        rads = radians(self.angle)
+        x = sin(rads) * self.speed
+        y = cos(rads) * self.speed
+        return pg.Vector2(x, y)
+
+    def rect(self) -> pg.Rect:
+        return pg.Rect(self.position, BALL_SIZE)
 
     def render(self, pg, screen):
-        rect = pg.Rect(self.position, BALL_SIZE)
-        pg.draw.rect(screen, WHITE, rect)
+        pg.draw.rect(screen, WHITE, self.rect())
 
     def update(self):
-        next_position = self.position + self.velocity
+        next_position = self.position + self.velocity()
         if next_position.x <= 0:
-            self.velocity.x *= -1
+            # That means the angle is between 180 and 360, for example 190, to bounce it has to become 90, 
+            # so 180 - angle - 180 = -angle
+            self.angle *= -1 # 180 - self.angle - 180
         if next_position.x + BALL_SIZE[0] >= SCREENRECT[2]:
-            self.velocity.x *= -1
+            # That means the angle is between 0 and 180, for example 100, to bounce it has to become 260
+            # -angle as well
+            self.angle *= -1
         if next_position.y <= 0:
-            self.velocity.y *= -1
+            # That means the angle is between 90 and 270, for example 100, to bounce it has to become 80
+            # 90 - angle + 90 = 
+            self.angle = 180 - self.angle
         if next_position.y + BALL_SIZE[1] >= SCREENRECT[3]:
-            self.velocity.y *= -1
+            # That means the angle is between 270 and 90, for example 30, to bounce it has to become 150
+            # 90 - angle + 90 = 
+            self.angle = 180 - self.angle
 
         self.position = next_position
+
+
+class CollisionSystem:
+    def update(self, ball, player_paddle, ai_paddle):
+        ball_rect = ball.rect()
+        if ball_rect.colliderect(player_paddle.rect):
+            ball.angle *= -1
+
+        if ball_rect.colliderect(ai_paddle.rect):
+            ball.angle *= -1
 
 
 def main(winstyle=0):
@@ -63,7 +90,10 @@ def main(winstyle=0):
     player_paddle = Paddle((10, (SCREENRECT[3] / 2) - (PADDLE_SIZE[1] / 2)))
     other_paddle = Paddle((SCREENRECT[2] - 20, (SCREENRECT[3] / 2) - (PADDLE_SIZE[1] / 2)))
     ball = Ball(position=pg.Vector2(x=SCREENRECT[2] / 2 - BALL_SIZE[0] / 2, y= SCREENRECT[3] / 2 - BALL_SIZE[1] / 2),
-                velocity=pg.Vector2(-5, -5))
+                speed=5,
+                angle=100)
+
+    collision_system = CollisionSystem()
 
     clock = pg.time.Clock()
 
@@ -81,6 +111,8 @@ def main(winstyle=0):
             player_paddle.move_down()
 
         ball.update()
+
+        collision_system.update(ball, player_paddle, other_paddle)
 
         screen.fill(BLACK)
 
